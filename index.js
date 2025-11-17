c0b4cfee5dd8d428ffd8503cc663f1021f809c92
MatiasJoaoSantos <joaomatiasf369@gmail.com>
2025-11-16 21:30:37 -0300

    extração da função

--- index.js
+++ index.js
@@ -9,26 +9,32 @@ function gerarFaturaStr (fatura, pecas) {
                             minimumFractionDigits: 2 }).format;
   
     for (let apre of fatura.apresentacoes) {
-      const peca = pecas[apre.id];
-      let total = 0;
-  
-      switch (peca.tipo) {
-      case "tragedia":
-        total = 40000;
+      const peca = pecas[apre.id];  
+
+      
+      function calcularTotalApresentacao(apre, peca) {
+        switch (peca.tipo) {
+          case "tragedia": {
+        let total = 40000;
         if (apre.audiencia > 30) {
           total += 1000 * (apre.audiencia - 30);
-        }
-        break;
-      case "comedia":
-        total = 30000;
+        } 
+        return total;
+          }
+          case "comedia": {
+        let total = 30000;
         if (apre.audiencia > 20) {
-           total += 10000 + 500 * (apre.audiencia - 20);
+          total += 10000 + 500 * (apre.audiencia - 20);
         }
         total += 300 * apre.audiencia;
-        break;
-      default:
-          throw new Error(`Peça desconhecia: ${peca.tipo}`);
+        return total;
+          }
+          default:
+        throw new Error(`Peça desconhecia: ${peca.tipo}`);
+        }
       }
+
+      let total = calcularTotalApresentacao(apre, peca);
   
       // créditos para próximas contratações
       creditos += Math.max(apre.audiencia - 30, 0);
